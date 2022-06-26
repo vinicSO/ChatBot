42039b45a1e924369d953ad13f4f2dc692293e1d
Vinicius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-06-26 12:07:50 -0300

    reducer bot

---
+++ src/reducers/botReducer.js
@@ -0,0 +1,73 @@
+import { nextId } from "react-id-generator";
+
+const bot = {
+	wait: true,
+	targetAction: "0",
+	actions: [
+		{
+			key: "0",
+			nome: "iniciar",
+			type: "options",
+			options: ["01", "02"]
+		},
+		{
+			key: "01",
+			nome: "OPTION 01",
+			type: "options",
+			options: ["011", "012", "013"]
+		},
+		{
+			key: "02",
+			nome: "OPTION 02",
+			type: "request",
+			request: ""
+		},
+		{
+			key: "011",
+			nome: "OPTION 01.1",
+			type: "process",
+			nextStep: "0111"
+		},
+		{
+			key: "0111",
+			nome: "OPTION 01.1.1",
+			type: "request",
+			request: ""
+		},
+		{
+			key: "012",
+			nome: "OPTION 01.2",
+			type: "process",
+			nextStep: "0121"
+		},
+		{
+			key: "0121",
+			nome: "OPTION 01.2.1",
+			type: "request",
+			request: ""
+		},
+		{
+			key: "013",
+			nome: "OPTION 01.3",
+			type: "process",
+			nextStep: "0131"
+		},
+		{
+			key: "0131",
+			nome: "OPTION 01.3.1",
+			type: "request",
+			request: ""
+		}
+	]
+}
+
+export default (state = bot, action) => {
+
+	switch (action.type) {
+	
+		default:
+      break;
+	}
+
+	return state;
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import messageReducer from "./messageReducer";
 import userReducer from "./userReducer";
+import botReducer from "./botReducer";
 
 const rootReducer = combineReducers({
-    messages: messageReducer,
-    user: userReducer
+  messages: messageReducer,
+  user: userReducer,
+  bot: botReducer
 });
 
 export default rootReducer;
