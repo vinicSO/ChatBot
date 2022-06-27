a8a97dd4e80740d96fd178e38c5d7a9420459a29
Vinicius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-06-26 22:24:31 -0300

    reafactor Message to function component

--- src/model/Message.js
+++ src/model/Message.js
@@ -1,53 +1,42 @@
-import { Component } from "react";
 import { StyleSheet } from "react-native";
 import { Box, HStack, Spacer, View, VStack, Text} from "native-base";
-import nextId from "react-id-generator";
 import Moment from "moment";
 import { handleMessageType } from "../components/MessageType";
 
-class Message extends Component {
-  constructor(props) {
-    super(props);
+function Message(props) {
+  
+  const timeStamp = Moment(props.time).format('HH:mm');
+  const content = props.content;
+  const me = props.me;
+  const config = props.me
+    ? { alignSelf: "flex-end", backgroundColor: "#4974b8" }
+    : { alignSelf: "flex-start", backgroundColor: "#8449d6" };
 
-    Moment.locale('br');
-    time = new Date();
-
-    this.id = nextId();
-    this.timeStamp = Moment(time).format('HH:mm');
-    this.content = props.content;
-    this.me = props.me;
-    this.config = props.me
-      ? { alignSelf: "flex-end", backgroundColor: "#4974b8" }
-      : { alignSelf: "flex-start", backgroundColor: "#8449d6" };
-  }
-
-  render() {
-    return (
-      <Box
-        _dark={{ borderColor: "muted.50" }}
-        borderColor="muted.800"
-        pl={["0", "4"]}
-        pr={["0", "5"]}
-        py="2"
-        style={styles.item}
-      >
-        <HStack space={[2, 3]} justifyContent="space-between">
-          {this.me && <Spacer />}
-          <VStack style={[styles.message, this.config]}>
-            <Text
-              fontSize="xs"
-              _dark={{ color: "warmGray.50" }}
-              color="white"
-              alignSelf="flex-end"
-            >
-              {this.timeStamp}
-            </Text>
-            {handleMessageType(this.content, this.props)}
-          </VStack>
-        </HStack>
-      </Box>
-    );
-  }
+  return (
+    <Box
+      _dark={{ borderColor: "muted.50" }}
+      borderColor="muted.800"
+      pl={["0", "4"]}
+      pr={["0", "5"]}
+      py="2"
+      style={styles.item}
+    >
+      <HStack space={[2, 3]} justifyContent="space-between">
+        {me && <Spacer />}
+        <VStack style={[styles.message, config]}>
+          <Text
+            fontSize="xs"
+            _dark={{ color: "warmGray.50" }}
+            color="white"
+            alignSelf="flex-end"
+          >
+            {timeStamp}
+          </Text>
+          {handleMessageType(content, props)}
+        </VStack>
+      </HStack>
+    </Box>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -58,7 +47,7 @@ const styles = StyleSheet.create({
   message: {
 		borderRadius: 10,
 		padding: 10,
-	},
+	}
 });
 
 export default Message;
