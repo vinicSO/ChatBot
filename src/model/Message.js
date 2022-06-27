17b629e1a7d4405f497fadbf2a9a7c6e6226146e
Vinicius Santos <vinicius.santos.oliv3ira@gmail.com>
2022-06-26 22:30:04 -0300

    remove module MessageType

--- src/model/Message.js
+++ src/model/Message.js
@@ -1,7 +1,9 @@
 import { StyleSheet } from "react-native";
-import { Box, HStack, Spacer, View, VStack, Text} from "native-base";
+import { Box, HStack, Spacer, VStack, Text } from "native-base";
+import nextId from "react-id-generator";
 import Moment from "moment";
-import { handleMessageType } from "../components/MessageType";
+import store from "../store";
+import { TouchableOpacity } from "react-native";
 
 function Message(props) {
   
@@ -11,6 +13,9 @@ function Message(props) {
   const config = props.me
     ? { alignSelf: "flex-end", backgroundColor: "#4974b8" }
     : { alignSelf: "flex-start", backgroundColor: "#8449d6" };
+
+  const handleClick = (action) => {
+  }
 
   return (
     <Box
@@ -32,7 +37,28 @@ function Message(props) {
           >
             {timeStamp}
           </Text>
-          {handleMessageType(content, props)}
+          {content.map((message) => 
+            <VStack key={nextId()}>
+              {
+                message.type == "text" &&
+                <Text
+                  color="white"
+                  style={{textAlign: (me) ? "right":"left"}}
+                >
+                  {message.text}
+                </Text>
+              }
+              {
+                message.type == "button" &&
+                <TouchableOpacity
+                  style={styles.buttonMessage}
+                  onPress={() => handleClick(message.action)}
+                >
+                  <Text style={styles.textButtonMessage}>{message.text}</Text>
+                </TouchableOpacity>
+              }
+            </VStack>
+          )}
         </VStack>
       </HStack>
     </Box>
@@ -47,7 +73,22 @@ const styles = StyleSheet.create({
   message: {
 		borderRadius: 10,
 		padding: 10,
+	},
+  buttonMessage: {
+		backgroundColor: "#fff",
+		width: 250,
+		height: 30,
+		borderRadius: 20,
+		margin: 5,
+		borderColor: "#79a8f2",
+		borderWidth: 1
+	},
+	textButtonMessage: {
+		flex: 1,
+		color: "#4974b8",
+		textAlign: "center",
+		textAlignVertical: "center"
 	}
 });
 
-export default Message;
+export {Message, createMessage};
